766d4ac577cbf43bcca6d0891a5c1d2911b55e2e
MustapheMS <moztaphem9@gmail.com>
2023-06-13 20:31:34 +0300

    Done Get First Name

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Good Luck 💪🏾
+
+// Done 💯😎
+const getFirstNames =  ([["Ahmed","Aisha","Amina"],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]]) 
+const groups = [["Ahmed","Aisha","Amina"] ,["Jaamac","Khadijah","Layla"], ["Mohamed","Ali"]]
+console.log(groups[0][0],groups[1][0],groups[2][0])
